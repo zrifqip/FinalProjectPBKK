1ae6cbdab415d6b82025156622546869d1024fb6
Muhammad Zufarrifqi Prakoso <zufarrifqi46@Gmail.com>
2023-10-03 11:48:17 +0700

    feat: add tailwind

--- resources/views/event/index.blade.php
+++ resources/views/event/index.blade.php
@@ -4,19 +4,43 @@
         <meta charset="UTF-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <title>Document</title>
+        @vite('resources/css/app.css')
     </head>
     <body>
-        <ul>
-            @foreach ($events as $event)
-            <li>
-                <div>
-                    {{ $event->id }}
-                    {{ $event->name }}
-                    {{ $event->description }}
-                    {{ $event->startDate }}
-                </div>
-            </li>
-            @endforeach
-        </ul>
+        <main class="min-h-screen">
+            <ul class="flex flex-col justify-center">
+                @foreach ($events as $event)
+                <li>
+                    <div class="max-w-sm w-full lg:max-w-full lg:flex pt-px">
+
+                        <div
+                            class="border-r border-b border-l border-gray-400 lg:border-l-0 lg:border-t lg:border-gray-400 bg-white rounded-b lg:rounded-b-none lg:rounded-r p-4 flex flex-col justify-between leading-normal"
+                        >
+                            <div class="mb-8">
+                                <div
+                                    class="text-gray-900 font-bold text-xl mb-2"
+                                >
+                                    {{$event->name}}
+                                </div>
+                                <p class="text-gray-700 text-base">
+                                    {{$event->description}}
+                                </p>
+                            </div>
+                            <div class="flex items-center">
+                                <div class="text-sm">
+                                    <p class="text-gray-900 leading-none">
+                                        {{$event->organizer}}
+                                    </p>
+                                    <p class="text-gray-600">
+                                        {{$event->startDate}}
+                                    </p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </li>
+                @endforeach
+            </ul>
+        </main>
     </body>
 </html>
